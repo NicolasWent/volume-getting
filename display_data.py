637f39ba965458e9c2ccacb9666338ba68f8eb3f
Nicolas Went <nicolas.went@epita.fr>
2021-12-07 00:03:06 +0100

    [Arguments] Added arguments

--- display_data.py
+++ display_data.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+import time
 import matplotlib.pyplot as plt
 
 
@@ -7,11 +8,12 @@ def to_datetime(to_convert: int) -> datetime:
     return datetime.fromtimestamp(to_convert / 1000.0)
 
 
-def display_data(data):
+def display_data(data, since, amount):
     timestamps = []
     volumes = []
+    t_minus = int(time.time()) - 1000 * 3600 * 24 * 30 * 12 * since
     for t in data:
-        if t > 1542408043594:
+        if t > t_minus:
             timestamps.append(to_datetime(t))
             volumes.append(data[t])
 
@@ -19,6 +21,6 @@ def display_data(data):
     plt.plot(timestamps, volumes)
     plt.xlabel("Dates")
     plt.ylabel("Volumes")
-    plt.title("Evolution of the volume of the 500 best actual crypto over time")
+    plt.title(f"Evolution of the volume of the {amount} best actual crypto over time")
     plt.grid(axis="y")
     plt.savefig("volumes.png")
--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+import argparse
+
 from data_computing import compute_data
 from data_getting import get_data
 from display_data import display_data
 
 
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Get the evolution of the volume of the best actual cryptos")
+
+    parser.add_argument("-a", "--amount", type=int, default=500, help="The amount of crypto that we should use")
+    parser.add_argument("-s", '--since', type=int, default=3, help="From how much years should we get the data? "
+                                                                   "default = 3")
+    return parser.parse_args()
+
+
 def main():
-    data = get_data(500)
+    args = parse_arguments()
+    data = get_data(args.amount)
     data = compute_data(data)
-    display_data(data)
+    display_data(data, args.since, args.amount)
 
 
 if __name__ == "__main__":
